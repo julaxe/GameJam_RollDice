84f54b59580db6a502fcabc2571334a2f98106a7
MichaelShular <60903307+MichaelShular@users.noreply.github.com>
2022-07-16 10:29:24 -0400

    UI dice

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,7 +8,9 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject m_playerDiceInfo;
     [SerializeField] GameObject m_enemyDiceInfo;
     private GameManager m_gameManager;
-    [SerializeField] List<Image> m_playerDiceSidesImageList;
+    [SerializeField] List<RawImage> m_playerDiceSidesImageList;
+    [SerializeField] List<GameObject> m_playerButtonList;
+
 
 
     private bool m_playerPanelActive;
@@ -33,11 +35,35 @@ public class UIManager : MonoBehaviour
 
     public void OpenDicePanel(int whichButton)
     {
+        //Resetting buttons to not be visable 
+        foreach (GameObject button in m_playerButtonList)
+        {
+            button.SetActive(false);
+        }
+
+
+
         switch (whichButton)
         {
             case 0:
                 m_playerDiceInfo.SetActive(!m_playerPanelActive);
                 m_playerPanelActive = !m_playerPanelActive;
+
+                for (int i = 0; i < m_gameManager.GetPlayer().GetComponent<DiceHolder>().m_actionDice.Count; i++)
+                {
+                    m_playerButtonList[i].SetActive(true);
+                }
+
+                Material[] temp = m_gameManager.GetPlayer().GetComponent<DiceHolder>().m_actionDice[0].GetComponent<MeshRenderer>().materials;
+                
+                m_playerDiceSidesImageList[0].texture = temp[3].mainTexture;
+                m_playerDiceSidesImageList[1].texture = temp[0].mainTexture;
+                m_playerDiceSidesImageList[2].texture = temp[1].mainTexture;
+                m_playerDiceSidesImageList[3].texture = temp[4].mainTexture;
+                m_playerDiceSidesImageList[4].texture = temp[2].mainTexture;
+                m_playerDiceSidesImageList[5].texture = temp[6].mainTexture;
+
+
 
                 break;
             case 1:
