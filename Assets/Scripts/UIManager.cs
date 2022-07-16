a6f0bde58843862e4d8e81900d766808365a62ca
MichaelShular <60903307+MichaelShular@users.noreply.github.com>
2022-07-15 20:35:38 -0400

    Fixed Dice Points, Added Dice Walls and Start of dice info ui

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_playerDiceInfo;
+    [SerializeField] GameObject m_enemyDiceInfo;
+    private bool m_playerPanelActive;
+    private bool m_enemyPanelActive;
+
+    private void Awake()
+    {
+        m_playerPanelActive = m_enemyPanelActive = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDicePanel(int whichButton)
+    {
+        switch (whichButton)
+        {
+            case 0:
+                m_playerDiceInfo.SetActive(!m_playerPanelActive);
+                m_playerPanelActive = !m_playerPanelActive;
+
+                break;
+            case 1:
+                m_playerDiceInfo.SetActive(!m_playerPanelActive);
+                m_playerPanelActive = !m_playerPanelActive;
+
+                break;
+            case 2:
+                m_playerDiceInfo.SetActive(!m_playerPanelActive);
+                m_playerPanelActive = !m_playerPanelActive;
+
+                break;
+            case 3:
+                m_enemyDiceInfo.SetActive(!m_enemyPanelActive);
+                m_enemyPanelActive = !m_enemyPanelActive;
+
+                break;
+            case 4:
+                m_enemyDiceInfo.SetActive(!m_enemyPanelActive);
+                m_enemyPanelActive = !m_enemyPanelActive;
+
+                break;
+            case 5:
+                m_enemyDiceInfo.SetActive(!m_enemyPanelActive);
+                m_enemyPanelActive = !m_enemyPanelActive;
+
+                break;
+
+
+            default:
+                break;
+        }
+        
+
+    }
+
+
+}
+
+
